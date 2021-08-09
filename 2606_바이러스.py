5e0423810bdc5a696260c474496bb5826605b534
soobin519 <soobin0519@naver.com>
2021-08-09 23:57:57 +0900

    8.9

---
+++ 2606_바이러스.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n, connect = int(input()), int(input())
+
+dic={}
+for i in range(n):
+    dic[i+1] = set()
+
+for j in range(connect):
+    a, b = map(int,input().split())
+    dic[a].add(b)
+    dic[b].add(a)
+
+#bfs
+def bfs(dic, start):
+    queue = [start]
+    while queue:
+        for i in dic[queue.pop()]:
+            if i not in visited:
+                visited.append(i)
+                queue.append(i)
+
+visited = []
+bfs(dic,1)
+print(len(visited)-1)
+    
